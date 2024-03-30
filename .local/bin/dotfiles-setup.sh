c503b54af2b3600b1619f1f539086a5c139b41c1
Kye Davey <kye.j.davey@gmail.com>
2024-03-31 09:53:42 +1100

    update dotfiles repo url

--- .local/bin/dotfiles-setup.sh
+++ .local/bin/dotfiles-setup.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:kyedavey/.dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/kyedavey/.dotfiles.git $HOME/.dotfiles
 mkdir -p .dotfiles-backup
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 if [ $? = 0 ]; then
