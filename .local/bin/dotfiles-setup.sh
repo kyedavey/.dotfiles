39aa7137138a2a7e7f1fb15e54b49266c3e6a666
Kye Davey <kye.j.davey@gmail.com>
2024-03-30 17:45:39 +1100

    fix formatting

--- .local/bin/dotfiles-setup.sh
+++ .local/bin/dotfiles-setup.sh
@@ -4,9 +4,9 @@ mkdir -p .dotfiles-backup
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+else
+  echo "Backing up pre-existing dot files.";
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
